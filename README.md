1734af969d747086db2fcbf4ce68db008ae1fee1
arijan <arijan@gmail.com>
2016-10-23 16:26:00 +0200

    code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PythonCards
-Playing cards in Python3
+Playing cards in Python3 demo
+## what is it for
+It's a simple demo on how to learn to program Python with emphasis on the following:
+* object interface design
+* object hirearchy design
+* usability
+* optimisation
--- cards2.py
+++ cards2.py
@@ -1,7 +1,7 @@
 # cards demo
 
 # lets have a list of ranks and suits
-ranks = list(map(str, range(2,11))) + list('JQKA')
+ranks = list(map(str, range(2, 11))) + list('JQKA')
 suits = list('SDCH')
 suitSyms = dict(zip(suits, list('♠♦♣♥')))
 
@@ -11,23 +11,26 @@ for v in ranks:
 for v in suits:
     locals()[v] = v
 
-# Singleton metaclass
-class SingletonOfEachKind(type):
+
+class ObjectSameForGivenParameters(type):
+    """ Object with same setup parameters will be same object not a new one."""
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         key = args + tuple(kwargs)
         if key not in cls._instances:
-            cls._instances[key] = super(SingletonOfEachKind, cls).__call__(*args, **kwargs)
+            cls._instances[key] = super(ObjectSameForGivenParameters, cls).__call__(*args, **kwargs)
         return cls._instances[key]
     
-class Card(metaclass=SingletonOfEachKind):
+
+class Card(metaclass=ObjectSameForGivenParameters):
     """ Card(rank, suite) is a single card """
     def __init__(self, r, s):
         if r in ranks and s in suits:
             self.__rank = r
             self.__suit = s
         else:
-            raise LookupError('Rank {} or suit {} does not exsist.'.format(r, s))
+            raise LookupError('Rank {} or suit {} does not exist.'.format(r, s))
 
     @property
     def rank(self):
@@ -46,12 +49,17 @@ class Card(metaclass=SingletonOfEachKind):
 from collections import Sequence
 import random
 
+
 class Deck(Sequence):
     """ Deck of cards """
     def __init__(self):
+        self.emptyDeck()
+
+    def emptyDeck(self):
         self.cards = []
+        return self
         
-    def FullDeck(self):
+    def fullDeck(self):
         self.cards = [Card(r, s) for s in suits for r in ranks]
         return self
 
@@ -71,11 +79,11 @@ class Deck(Sequence):
         if c in self:
             self.cards.remove(c)
         else:
-            raise LookupError('Card {} not in fromDeck {}.'.format(c, self.cards))
+            raise LookupError('Card {} not in remove deck {}.'.format(c, self.cards))
 
     def append(self, c):
         if c in self:
-            raise LookupError('Card {} already in toDeck {}.'.format(c, self.cards))
+            raise LookupError('Card {} already in append deck {}.'.format(c, self.cards))
         else:
             self.cards.append(c)
 
@@ -91,23 +99,36 @@ class Deck(Sequence):
     def topCard(self):
         return self.cards[0]
         
+
 def TakeCard(card, fromDeck, toDeck):
     fromDeck.remove(card)
     toDeck.append(card)
 
+
+def TopCard(deck):
+    return deck.topCard()
+
+
 def DealHand(deck, hand):
     for c in range(0, 5):
-        TakeCard(deck.topCard(), deck, hand)
-        
-d=Deck().FullDeck()
-h=Deck()
-TakeCard(Card(A, S), d, h)
-print(Card(A, S) in d)
-print(Card(A, S) in h)
-h2=Deck()
-DealHand(d, h2)
+        TakeCard(TopCard(deck), deck, hand)
+
+
+def Shuffle(deck):
+    deck.shuffle()
+
+# simple tests
+deck1 = Deck().fullDeck()
+hand1 = Deck().emptyDeck()
+TakeCard(Card(A, S), deck1, hand1)
+print(Card(A, S) in deck1)
+print(Card(A, S) in hand1)
+
+h2 = Deck()
+DealHand(deck1, h2)
 print(h2)
-h3=Deck()
-d.shuffle()
-DealHand(d, h3)
+
+h3 = Deck().emptyDeck()
+Shuffle(deck1)
+DealHand(deck1, h3)
 print(h3)
